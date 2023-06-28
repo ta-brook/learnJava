72e0141227c0eee03effd32c7875caf367519340
Tanawat Jirawattanakul <tanawat.jrw@gmail.com>
2023-06-28 23:53:22 +0700

    add example of java .class -> usage of java vs javac command

---
+++ Example/src/ClientProgram.java
@@ -0,0 +1,7 @@
+public class ClientProgram {
+    public static void main(String args[]){
+        Multiplier mult = new Multiplier();
+        int result = mult.multiply(2, 3, 4);
+        System.out.println(result);
+    }
+}
